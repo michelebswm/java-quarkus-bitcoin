c35cb102cf7523a5fb65dd954b993797f6434009
michelebswm <michele.fenix.17@gmail.com>
2024-06-29 19:41:21 -0300

    feat: adiciona try catch no UsuarioResource

--- src/main/java/br/com/alura/resource/UsuarioResource.java
+++ src/main/java/br/com/alura/resource/UsuarioResource.java
@@ -3,12 +3,14 @@ package br.com.alura.resource;
 import br.com.alura.exception.ApplicationServiceException;
 import br.com.alura.model.Usuario;
 import br.com.alura.service.UsuarioService;
+import br.com.alura.util.message.MessageService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/usuarios")
 public class UsuarioResource {
@@ -18,7 +20,15 @@ public class UsuarioResource {
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public void create(Usuario usuario) throws ApplicationServiceException {
-        usuarioService.create(usuario);
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(Usuario usuario)  {
+        try {
+            usuarioService.create(usuario);
+        }catch (ApplicationServiceException ase){
+            return Response.status(ase.getStatusCode()).entity(new MessageService(ase.getMessage(),
+                    ase.getErrorList())).build();
+        }
+
+        return Response.status(Response.Status.CREATED).build();
     }
 }
