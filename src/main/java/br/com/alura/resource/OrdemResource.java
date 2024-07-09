2dabf893f80c95f3049e659a10656232231741c2
michelebswm <michele.fenix.17@gmail.com>
2024-07-09 20:49:32 -0300

    feat: adiciona documentacao swagger openapi

--- src/main/java/br/com/alura/resource/OrdemResource.java
+++ src/main/java/br/com/alura/resource/OrdemResource.java
@@ -2,7 +2,6 @@ package br.com.alura.resource;
 
 import br.com.alura.dto.OrdemRequestDTO;
 import br.com.alura.dto.OrdemResponseDTO;
-import br.com.alura.dto.UsuarioResponseDTO;
 import br.com.alura.exception.ApplicationServiceException;
 import br.com.alura.service.OrdemService;
 import br.com.alura.util.message.MessageService;
@@ -15,11 +14,25 @@ import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.SecurityContext;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.ParameterIn;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.headers.Header;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Path("/ordens")
+@Tag(name = "ordens", description = "Ordens de Compra e Venda")
+@APIResponse(responseCode = "401", description = "Acesso não autorizado")
+@APIResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso.")
+@APIResponse(responseCode = "500", description = "Tratamento de erro interno",
+        content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = MessageService.class)))
 public class OrdemResource {
 
     @Inject
@@ -29,6 +42,9 @@ public class OrdemResource {
     @RolesAllowed("user")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = "Inclusão de registro")
+    @APIResponse(responseCode = "201", description = "Registro incluído.")
+    @APIResponse(responseCode = "400", description = "Parâmetro(s) inválido(s).")
     public Response create(@Context SecurityContext securityContext, OrdemRequestDTO ordem){
         try{
             ordemService.create(securityContext, ordem);
@@ -44,6 +60,14 @@ public class OrdemResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @RolesAllowed("admin")
+    @Operation(description = "Listagem paginada de registros")
+    @Parameter(name = "page", description = "Página atual", in = ParameterIn.QUERY)
+    @Parameter(name = "size", description = "Quantidade de ocorrências que devem ser retornadas", in = ParameterIn.QUERY)
+    @Parameter(name = "sort", description = "Nome do atributo utilizado para ordenação das ocorrências", in = ParameterIn.QUERY)
+    @Parameter(name = "order", description = "Sentido da ordenação: ASC|DESC", in = ParameterIn.QUERY)
+    @APIResponse(responseCode = "200", description = "Ok",
+            headers = @Header(name = "Pagination-Count", description = "Quantidade total de páginas",
+                    schema = @Schema(type = SchemaType.INTEGER)))
     public Response listarOrdens(
             @QueryParam("page") @DefaultValue("0") Integer page,
             @QueryParam("size") @DefaultValue("10") Integer size,
--- src/main/java/br/com/alura/resource/UsuarioResource.java
+++ src/main/java/br/com/alura/resource/UsuarioResource.java
@@ -12,11 +12,25 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.ParameterIn;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.headers.Header;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Path("/usuarios")
+@Tag(name = "usuarios", description = "Operações referentes aos usuários")
+@APIResponse(responseCode = "401", description = "Acesso não autorizado")
+@APIResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso.")
+@APIResponse(responseCode = "500", description = "Tratamento de erro interno",
+        content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = MessageService.class)))
 public class UsuarioResource {
 
     @Inject
@@ -26,6 +40,9 @@ public class UsuarioResource {
     @PermitAll
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = "Inclusão de registro")
+    @APIResponse(responseCode = "201", description = "Registro incluído.")
+    @APIResponse(responseCode = "400", description = "Parâmetro(s) inválido(s).")
     public Response create(Usuario usuario)  {
         try {
             usuarioService.create(usuario);
@@ -39,6 +56,14 @@ public class UsuarioResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = "Listagem paginada de registros")
+    @Parameter(name = "page", description = "Página atual", in = ParameterIn.QUERY)
+    @Parameter(name = "size", description = "Quantidade de ocorrências que devem ser retornadas", in = ParameterIn.QUERY)
+    @Parameter(name = "sort", description = "Nome do atributo utilizado para ordenação das ocorrências", in = ParameterIn.QUERY)
+    @Parameter(name = "order", description = "Sentido da ordenação: ASC|DESC", in = ParameterIn.QUERY)
+    @APIResponse(responseCode = "200", description = "Ok",
+            headers = @Header(name = "Pagination-Count", description = "Quantidade total de páginas",
+                    schema = @Schema(type = SchemaType.INTEGER)))
     public Response listarUsuarios(
             @QueryParam("page") @DefaultValue("0") Integer page,
             @QueryParam("size") @DefaultValue("10") Integer size,
@@ -60,6 +85,9 @@ public class UsuarioResource {
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(description = "Alteração de registro")
+    @APIResponse(responseCode = "204", description = "Registro alterado")
+    @APIResponse(responseCode = "400", description = "Parâmetro(s) inválido(s)")
     public Response alterar(Usuario usuario){
         try{
             usuarioService.alterar(usuario);
@@ -73,6 +101,10 @@ public class UsuarioResource {
     @DELETE
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = "Exclusão de registro")
+    @Parameter(name = "id", description = "Identificador do registro", in = ParameterIn.PATH)
+    @APIResponse(responseCode = "204", description = "Registro excluído")
+    @APIResponse(responseCode = "400", description = "Parâmetro(s) inválido(s)")
     public Response excluir(@PathParam("id") Long id){
         try {
             usuarioService.excluir(id);
